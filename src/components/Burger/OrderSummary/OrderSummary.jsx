66151028723a92201f136fcc0fdc2bb9f18a1dbb
Anand raja <meetanandns@gmail.com>
2022-05-20 08:47:18 +0000

    total price at orderpage

--- src/components/Burger/OrderSummary/OrderSummary.jsx
+++ src/components/Burger/OrderSummary/OrderSummary.jsx
@@ -23,6 +23,9 @@ const orderSummary = (props) => {
       <ul>
         { ingredientsSummary }
       </ul>
+      <p>
+        <strong>Total Price: {props.price}</strong>
+      </p>
       <p>Continue to checkout?</p>
       <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
       <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
