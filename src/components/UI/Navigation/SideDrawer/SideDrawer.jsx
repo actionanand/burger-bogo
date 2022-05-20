cd056e9c44ce2dc015f1d00945e98762e3b39fcf
Anand raja <meetanandns@gmail.com>
2022-05-20 18:11:17 +0000

    reordering folder structure

--- src/components/UI/Navigation/SideDrawer/SideDrawer.jsx
+++ src/components/UI/Navigation/SideDrawer/SideDrawer.jsx
@@ -5,7 +5,7 @@ import classes from './SideDrawer.module.css';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import Logo from '../../../Logo/Logo';
 import Backdrop from '../../Backdrop/Backdrop';
-import Aux from '../../../../hoc/Aux';
+import Aux from '../../../../hoc/Aux/Aux';
 
 const sideDrawer = (props) => {
 
