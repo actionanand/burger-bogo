5234ea0cc3cfb978b295f089331f5f2b73f0dcd2
Anand raja <meetanandns@gmail.com>
2022-05-21 14:04:03 +0000

    clean up after http call

--- src/hoc/withErrorHandler/withErrorHandler.jsx
+++ src/hoc/withErrorHandler/withErrorHandler.jsx
@@ -13,14 +13,19 @@ const withErrorhandler = (WrappedComponent, axios) => {
     };
 
     componentDidMount() {
-      axios.interceptors.request.use(req => {
+      this.reqInterceptor = axios.interceptors.request.use(req => {
         this.setState({error: null});
         return req;
       });
 
-      axios.interceptors.response.use(resp => resp, error => {
+      this.respInterceptor = axios.interceptors.response.use(resp => resp, error => {
         this.setState({error});
       });
+    }
+
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.request.eject(this.respInterceptor);
     }
 
     errorConfirmationHandler = () => {
