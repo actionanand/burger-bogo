3029a1bddf81ddf7315857db684f5214ebe9e801
Anand raja <meetanandns@gmail.com>
2022-05-23 13:13:06 +0000

    query params

--- src/containers/Checkout/Checkout.jsx
+++ src/containers/Checkout/Checkout.jsx
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import withRouter from '../../hoc/withRouter/withRouter';
 
 class Checkout extends Component {
 
   state = {
     ingredients: {
-      salad: 1,
-      meat: 1,
-      cheese: 2,
-      bacon: 1
+      salad: 0,
+      meat: 0,
+      cheese: 0,
+      bacon: 0
     }
   };
+
+  constructor() {
+    super();
+    this.checkoutCancelledHandler = this.checkoutCancelledHandler.bind(this);
+  }
+
+  componentDidMount() {
+    const query = new URLSearchParams(this.props.location.search);
+    console.log(query)
+    const ingredients = {};
+
+    for(let param of query.entries()) {
+      ingredients[param[0]] = +param[1];
+    }
+
+    // console.log(ingredients);
+
+    this.setState({ingredients});
+
+  }
+
+  checkoutCancelledHandler() {
+    this.props.navigate('/');
+  }
+
+  checkoutContinuedHandler = () => {
+    this.props.navigate('contact-data');
+  }
 
   render() {
     return (
       <div>
-        <CheckoutSummary ingredients={this.state.ingredients} />
+        <CheckoutSummary 
+          ingredients={this.state.ingredients}
+          checkoutCancelled={this.checkoutCancelledHandler}
+          checkoutContinued={this.checkoutContinuedHandler} />
       </div>
     );
   }
 }
 
-export default Checkout;
+export default withRouter(Checkout);
