870c549e70a329fa8252ce2f2a76f4c8fab03ffd
Anand raja <meetanandns@gmail.com>
2022-05-23 18:20:23 +0000

    orders section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Checkout/Orders/Orders';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
           <Routes>
             <Route path='/' element={<BurgerBuilder/>} />
             <Route path='/checkout/*' element={<Checkout/>} />
+            <Route path='/orders' element={<Orders/>} />
           </Routes>
         </Layout>
       </div>
