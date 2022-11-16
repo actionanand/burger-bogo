4bbe272393d5cef41ca8d21238ce4cf9f1fce56d
Anand raja <meetanandns@gmail.com>
2022-11-16 20:22:12 +0000

    refactoring

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const basePrice = 80; 
 
@@ -18,27 +19,27 @@ const INGREDIENT_PRICES = {
 
 const reducer = (state = initialState, action) => {
   switch(action.type) {
+
     case actionTypes.ADD_INGREDIENT:
-      return {
-        ...state,
-        ingredients: {
-          ...state.ingredients,
-          [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-        },
+      const addUpdatedIng = {[action.ingredientName]: state.ingredients[action.ingredientName] + 1};
+      const addUpdatedIngs = updateObject(state.ingredients, addUpdatedIng);
+      const addUpdatedState = {
+        ingredients: addUpdatedIngs,
         totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
       };
+      return updateObject(state, addUpdatedState);
+
     case actionTypes.REMOVE_INGREDIENT:
-      return {
-        ...state,
-        ingredients: {
-          ...state.ingredients,
-          [action.ingredientName]: state.ingredients[action.ingredientName] - 1
-        },
+      const subUpdatedIng = {[action.ingredientName]: state.ingredients[action.ingredientName] - 1};
+      const subUpdatedIngs = updateObject(state.ingredients, subUpdatedIng);
+      const subUpdatedState = {
+        ingredients: subUpdatedIngs,
         totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
       };
+      return updateObject(state, subUpdatedState);
+
     case actionTypes.SET_INGREDIENTS:
-      return {
-        ...state,
+      return updateObject(state, {
         ingredients: {
           salad: action.ingredients.salad,
           bacon: action.ingredients.bacon,
@@ -47,15 +48,58 @@ const reducer = (state = initialState, action) => {
         },
         error: false,
         totalPrice: basePrice
-      };
+      });
+
     case actionTypes.FETCH_INGREDIENTS_FAILED:
-      return {
-        ...state,
-        error: true
-      };
+      return updateObject(state, { error: true });
+
     default:
       return state;
   }
 };
+
+// direct way without utility function
+
+// const reducer = (state = initialState, action) => {
+//   switch(action.type) {
+//     case actionTypes.ADD_INGREDIENT:
+//       return {
+//         ...state,
+//         ingredients: {
+//           ...state.ingredients,
+//           [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+//         },
+//         totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+//       };
+//     case actionTypes.REMOVE_INGREDIENT:
+//       return {
+//         ...state,
+//         ingredients: {
+//           ...state.ingredients,
+//           [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+//         },
+//         totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
+//       };
+//     case actionTypes.SET_INGREDIENTS:
+//       return {
+//         ...state,
+//         ingredients: {
+//           salad: action.ingredients.salad,
+//           bacon: action.ingredients.bacon,
+//           cheese: action.ingredients.cheese,
+//           meat: action.ingredients.meat
+//         },
+//         error: false,
+//         totalPrice: basePrice
+//       };
+//     case actionTypes.FETCH_INGREDIENTS_FAILED:
+//       return {
+//         ...state,
+//         error: true
+//       };
+//     default:
+//       return state;
+//   }
+// };
 
 export default reducer;
