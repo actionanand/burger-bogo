35b0ad5c05be5b6a1a8b707c03a13c89c69e4a0e
Anand raja <meetanandns@gmail.com>
2022-11-17 14:10:41 +0000

    working form

---
+++ src/containers/Auth/Auth.jsx
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+
+import classes from './Auth.module.css';
+import Input from '../../components/UI/Input/Input';
+import Button from '../../components/UI/Button/Button';
+
+class Auth extends Component {
+  state = {
+    controls: {
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'E-mail address'
+        },
+        value: '',
+        validation: {
+          required: true,
+          isEmail: true
+        },
+        valid: false,
+        touched: false
+      },
+      password: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'password',
+          placeholder: 'Your Password'
+        },
+        value: '',
+        validation: {
+          required: true,
+          minLength: 7
+        },
+        valid: false,
+        touched: false
+      },
+    }
+  };
+
+  checkValidity(value, rules) {
+    let isValid = true;
+
+    if(!rules) {
+      return;
+    }
+
+    if(rules.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+
+    if(rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if(rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+
+    if (rules.isEmail) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test(value) && isValid
+    }
+
+    if (rules.isNumeric) {
+      const pattern = /^\d+$/;
+      isValid = pattern.test(value) && isValid
+    }
+
+
+    return isValid;
+  }
+
+  inputChangedHandler = (event, controlName) => {
+    const updatedControls = {
+      ...this.state.controls,
+      [controlName]: {
+        ...this.state.controls[controlName],
+        value: event.target.value,
+        valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+        touched: true
+      }
+    };
+
+    this.setState({controls: updatedControls});
+  };
+
+  render() {
+    const formElemenrArray = [];
+
+    for (let key in this.state.controls) {
+      formElemenrArray.push({
+        id: key,
+        config: this.state.controls[key]
+      });
+    }
+
+    const form = formElemenrArray.map(formEl => (
+      <Input key={formEl.id}
+      elementType={formEl.config.elementType} 
+      elementConfig={formEl.config.elementConfig} 
+      value={formEl.config.value}
+      invalid={!formEl.config.valid}
+      shouldValidate={formEl.config.validation}
+      touched={formEl.config.touched}
+      changed={(event) => this.inputChangedHandler(event, formEl.id)} />
+    ));
+
+    
+
+    return (
+      <div className={classes.Auth}>
+        <form>
+          {form}
+          <Button btnType='Success'> Submit </Button>
+        </form>
+      </div>
+    );
+  };
+}
+
+export default Auth;
