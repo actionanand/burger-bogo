5b3173f561f36a4900cceef3407669538ce98a47
Anand raja <46064269+actionanand@users.noreply.github.com>
2022-11-19 10:28:15 +0000

    outsourcing validation

--- src/containers/Checkout/ContactData/ContactData.jsx
+++ src/containers/Checkout/ContactData/ContactData.jsx
@@ -12,6 +12,7 @@ import withRouter from '../../../hoc/withRouter/withRouter';
 import withErrorhandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../../store/actions/index';
 import { updateObject } from '../../../shared/utility';
+import { checkValidity } from '../../../shared/validation';
 
 
 class ContactData extends Component {
@@ -106,38 +107,6 @@ class ContactData extends Component {
     isFormValid: false
   };
 
-  checkValidity(value, rules) {
-    let isValid = true;
-
-    if(!rules) {
-      return;
-    }
-
-    if(rules.required) {
-      isValid = value.trim() !== '' && isValid;
-    }
-
-    if(rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid;
-    }
-
-    if(rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid;
-    }
-
-    if (rules.isEmail) {
-      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-      isValid = pattern.test(value) && isValid
-    }
-
-    if (rules.isNumeric) {
-      const pattern = /^\d+$/;
-      isValid = pattern.test(value) && isValid
-    }
-
-
-    return isValid;
-  }
 
   orderHandler = (event) => {
     event.preventDefault();
@@ -163,7 +132,7 @@ class ContactData extends Component {
     
     const updatedFormEl = updateObject(this.state.orderForm[inputIdentifier], {
       value: event.target.value,
-      valid: this.checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
+      valid: checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation),
       touched: true
     });
 
@@ -172,7 +141,7 @@ class ContactData extends Component {
     });
     
     let isFormValid = true;
-    
+
     for(let formIdentifier in updatedOrderForm) {
       isFormValid = updatedOrderForm[formIdentifier].valid && isFormValid;
     }
