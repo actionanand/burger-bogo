043460555f964c47371bb8c50e2db835827496d9
Anand raja <46064269+actionanand@users.noreply.github.com>
2022-11-20 07:29:02 +0000

    auth saga

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,3 @@
-import axios from 'axios';
-
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -24,11 +22,6 @@ export const authFail = error => {
 }
 
 export const logout = () => {
-  // clearTimeout(timeOutId);
-  // localStorage.removeItem('expirationDate');
-  // localStorage.removeItem('token');
-  // localStorage.removeItem('userId');
-
   return {
     type: actionTypes.AUTH_INITIATE_LOGOUT
   };
@@ -77,35 +70,10 @@ export const authCheckState = () => {
 
 
 export const auth = (email, password, isSignUp = true) => {
-  const authUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:';
-  let authPath = 'signUp';
-
-  if (!isSignUp) {
-    authPath = 'signInWithPassword';
-  }
-
-  return dispatch => {
-    dispatch(authStart());
-    const authData = {
-      email,
-      password,
-      returnSecureToken: true
-    };
-
-    axios.post(authUrl + authPath + '?key=' + process.env.REACT_APP_AUTH_KEY, authData)
-      .then(resp => {
-        // console.log(resp);
-        const expirationDate = new Date(new Date().getTime() + (resp.data.expiresIn * 1000));
-        localStorage.setItem('token', JSON.stringify(resp.data.idToken));
-        localStorage.setItem('expirationDate', expirationDate);
-        localStorage.setItem('userId', JSON.stringify(resp.data.localId));
-
-        dispatch(authSuccess(resp.data.idToken, resp.data.localId));
-        dispatch(checkAuthTimeout(resp.data.expiresIn));
-      })
-      .catch(err => {
-        console.log(err);
-        dispatch(authFail(err.response.data.error));
-      })
+  return {
+    type: actionTypes.AUTH_USER,
+    email,
+    password,
+    isSignUp
   };
 }
--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,5 +1,7 @@
 import { put, delay } from 'redux-saga/effects';
 
+import axios from 'axios';
+
 import * as actionTypes from '../actions/actionTypes';
 import * as actions from '../actions/index';
 
@@ -26,4 +28,37 @@ export const didLogout = () => {
 export function* checkAuthTimeoutSaga(action) {
   yield delay(action.expirationTime * 1000);
   yield put(actions.logout());
+}
+
+export function* authSaga({isSignUp, email, password}) {
+  yield put(actions.authStart());
+  
+  const authUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:';
+  let authPath = 'signUp';
+
+  if (!isSignUp) {
+    authPath = 'signInWithPassword';
+  }
+
+  const authData = {
+    email,
+    password,
+    returnSecureToken: true
+  };
+
+
+  try {
+    const resp = yield axios.post(authUrl + authPath + '?key=' + process.env.REACT_APP_AUTH_KEY, authData)
+    
+    const expirationDate = yield new Date(new Date().getTime() + (resp.data.expiresIn * 1000));
+    yield localStorage.setItem('token', JSON.stringify(resp.data.idToken));
+    yield localStorage.setItem('expirationDate', expirationDate);
+    yield localStorage.setItem('userId', JSON.stringify(resp.data.localId));
+  
+    yield put(actions.authSuccess(resp.data.idToken, resp.data.localId));
+    yield put(actions.checkAuthTimeout(resp.data.expiresIn));
+  
+  } catch (err) {
+    yield put(actions.authFail(err.response.data.error));
+  }
 }
